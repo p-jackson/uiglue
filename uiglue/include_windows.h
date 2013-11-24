d18292879670f6fdb85f634806cd59a6146c67e3
Philip Jackson <p-jackson@live.com>
2013-11-24 19:59:42 +1300

    Using the cotire CMake module to automatically handle precompiled headers.

--- uiglue/include_windows.h
+++ uiglue/include_windows.h
@@ -1,3 +1,11 @@
+//===-- Windows.h wrapper -------------------------------------------------===//
+//
+// The definitions defined in Windows.h can be controlled using pre-processor
+// macros. To ensure consistent definitions, uiglue code includes this header
+// instead of including Windows.h directly.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef INCLUDE_WINDOWS_H
 #define INCLUDE_WINDOWS_H
 
@@ -5,6 +13,20 @@
 
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
+#define OEMRESOURCE
 #include <Windows.h>
+
+// If cotire (CMake module) includes Windows.h in it's precompiled header then
+// NOMINMAX will have no effect; min/max will already be defined by the
+// precompiled header.
+// These macros break code so get rid of them.
+
+#if defined(min)
+  #undef min
+#endif
+
+#if defined(max)
+  #undef max
+#endif
 
 #endif
--- uiglue/pch.h
+++
@@ -1,8 +0,0 @@
-#ifndef PCH_H
-#define PCH_H
-
-#include "include_windows.h"
-
-#include <boost/filesystem.hpp>
-
-#endif
