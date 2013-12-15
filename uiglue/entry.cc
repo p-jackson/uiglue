ec65051db8613ea80939e55a61b797c2173f933c
Philip Jackson <p-jackson@live.com>
2013-12-15 22:30:26 +1300

    Format entry.cc

--- uiglue/entry.cc
+++ uiglue/entry.cc
@@ -5,37 +5,34 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "resource.h"
-
 #include "computed.h"
 #include "main_view.h"
 #include "member_map.h"
+#include "resource.h"
 #include "observable.h"
 #include "view.h"
 #include "view_factory.h"
 
 #include "curt/curt.h"
-#include "curt/include_windows.h"
 #include "curt/util.h"
 
 #include <boost/algorithm/string/case_conv.hpp>
 #include <functional>
 #include <unordered_map>
 
-using uiglue::Computed;
-using uiglue::Observable;
-using uiglue::View;
 using std::string;
+using namespace curt;
+using namespace uiglue;
 
 // Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM) {
-  switch (message) {
+INT_PTR __stdcall aboutProc(HWND dlg, unsigned int msg, WPARAM wParam, LPARAM) {
+  switch (msg) {
   case WM_INITDIALOG:
     return 1;
 
   case WM_COMMAND:
     if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-      curt::endDialog(hDlg, LOWORD(wParam));
+      endDialog(dlg, LOWORD(wParam));
       return TRUE;
     }
     break;
@@ -68,15 +65,15 @@ private:
   Computed<string> greeting;
 
   void onExit(View& view) {
-    curt::destroyWindow(view.get());
+    destroyWindow(view.get());
   }
 
   void onAbout(View& view) {
-    curt::dialogBoxParam(curt::thisModule(), IDD_ABOUTBOX, view.get(), About, 0);
+    dialogBoxParam(thisModule(), IDD_ABOUTBOX, view.get(), aboutProc, 0);
   }
 
   void onModalGreeting(View& view) {
-    curt::messageBox(view.get(), greeting(), L"Greeting", MB_OK);
+    messageBox(view.get(), greeting(), L"Greeting", MB_OK);
   }
 
   string calculateGreeting() {
@@ -96,28 +93,33 @@ private:
 };
 
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE, _In_ LPTSTR, _In_ int show) {
+int APIENTRY wWinMain(
+  _In_ HINSTANCE hInst,
+  _In_opt_ HINSTANCE,
+  _In_ LPTSTR,
+  _In_ int show
+) {
   try {
     auto vm = MainViewModel{};
-    auto mainView = uiglue::makeMainView();
+    auto mainView = makeMainView();
 
     //uiglue::attachViewModel(vm, mainView.get());
 
-    curt::showWindow(mainView, show);
-    curt::updateWindow(mainView);
+    showWindow(mainView, show);
+    updateWindow(mainView);
 
-    auto accelTable = curt::loadAccelerators(hInst, IDC_UIGLUE);
+    auto accelTable = loadAccelerators(hInst, IDC_UIGLUE);
 
-    auto result = curt::pumpMessages(mainView, accelTable);
+    auto result = pumpMessages(mainView, accelTable);
 
     return static_cast<int>(result);
   }
   catch (std::exception& e) {
-    curt::messageBox(nullptr, e.what(), "Exception", MB_OK | MB_ICONERROR);
+    messageBox(nullptr, e.what(), "Exception", MB_OK | MB_ICONERROR);
     return 0;
   }
   catch (...) {
-    curt::messageBox(nullptr, "Unknown exception", "Exception", MB_OK | MB_ICONERROR);
+    messageBox(nullptr, "Unknown exception", "Exception", MB_OK | MB_ICONERROR);
     return 0;
   }
 }
