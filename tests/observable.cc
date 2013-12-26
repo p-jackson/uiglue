5b8ce4e6d63f855c4dfbbdfaf203da752ca54257
Philip Jackson <p-jackson@live.com>
2013-12-26 19:49:42 +1300

    Added forwarding constructors to Observable

--- tests/observable.cc
+++ tests/observable.cc
@@ -28,20 +28,91 @@ TEST(Observable, ctorDefault) {
   ASSERT_EQ(1, o2().count);
 }
 
+TEST(Observable, ctorForwarding) {
+  auto o1 = Observable<std::string>{ "test" };
+  ASSERT_EQ("test", o1());
+
+  auto o2 = Observable<std::pair<int, double>>{ 113, 3.14 };
+  ASSERT_EQ(std::make_pair(113, 3.14), o2());
+
+  struct Obj {
+    int count;
+    Obj(int c) : count{ c } {}
+  };
+  auto o3 = Observable<Obj>{ 3 };
+  ASSERT_EQ(3, o3().count);
+}
+
 TEST(Observable, ctorCopy) {
   auto o1 = Observable<int>{ 113 };
   auto o2 = o1;
   ASSERT_EQ(113, o2());
   ASSERT_EQ(o1(), o2());
 
+  static auto nextValue = 113;
   struct Obj {
-    bool copied = false;
-    Obj() = default;
-    Obj(const Obj&) : copied{ true } { }
-    bool operator==(const Obj&) { return false; }
+    int value;
+    Obj() : value{ nextValue++ } {}
   };
   auto o3 = Observable<Obj>{};
-  ASSERT_FALSE(o3().copied);
+  ASSERT_EQ(113, o3().value);
   auto o4 = o3;
-  ASSERT_TRUE(o4().copied);
+  ASSERT_EQ(113, o4().value);
+  ASSERT_EQ(114, nextValue);
+}
+
+TEST(Observable, ctorMove) {
+  auto o1 = Observable<int>{ 113 };
+  auto o2 = std::move(o1);
+  ASSERT_EQ(113, o2());
+
+  static auto nextValue = 113;
+  struct Obj {
+    int value;
+    Obj() : value{ nextValue++ } {}
+    Obj(const Obj&) { CONSTRUCTOR_FAIL(); }
+  };
+  auto o3 = Observable<Obj>{};
+  auto o4 = std::move(o3);
+  ASSERT_EQ(113, o4().value);
+  ASSERT_EQ(114, nextValue);
+}
+
+TEST(Observable, assignCopy) {
+  Observable<int> o1;
+  auto o2 = Observable<int>{ 113 };
+  o1 = o2;
+  ASSERT_EQ(113, o1());
+  ASSERT_EQ(o1(), o2());
+
+  static auto nextValue = 113;
+  struct Obj {
+    int value;
+    Obj() : value{ nextValue++ } {}
+  };
+  Observable<Obj> o3; // This increments nextValue;
+  auto o4 = Observable<Obj>{};
+  ASSERT_EQ(114, o4().value);
+  o3 = o4;
+  ASSERT_EQ(114, o3().value);
+  ASSERT_EQ(115, nextValue);
+}
+
+TEST(Observable, assignMove) {
+  Observable<int> o1;
+  auto o2 = Observable<int>{ 113 };
+  o1 = std::move(o2);
+  ASSERT_EQ(113, o1());
+
+  static auto nextValue = 113;
+  struct Obj {
+    int value;
+    Obj() : value{ nextValue++ } {}
+    Obj(const Obj&) { CONSTRUCTOR_FAIL(); }
+  };
+  auto o3 = Observable<Obj>{};
+  auto o4 = Observable<Obj>{};
+  o3 = std::move(o4);
+  ASSERT_EQ(114, o3().value);
+  ASSERT_EQ(115, nextValue);
 }
--- uiglue/observable.h
+++ uiglue/observable.h
@@ -9,8 +9,9 @@
 // it should be wrapped in an Observable. Observables control access and changes
 // to the wrapped value and allow others to subscribe to the changes.
 //
-// An Observable<T> is copy construcible if T is, move constructible if T is,
-// etc.
+// An Observable<T> is default constructible if T is default constructible and
+// is copy constructible if T is copy constructible. Observable's are always
+// move constructible.
 // To be wrapped in an Observable T must be == comparable. The Observable uses
 // == in the setter to see if the value really changed before notifying the
 // subscribers.
@@ -24,6 +25,7 @@
 #include <boost/variant.hpp>
 #include <functional>
 #include <set>
+#include <type_traits>
 #include <unordered_map>
 
 namespace uiglue {
@@ -89,10 +91,9 @@ class TypedObservable : public IUntypedObservable,
   };
 
 public:
-  TypedObservable() = default;
-
-  TypedObservable(T t)
-    : m_value{ std::move(t) }
+  template<class... P>
+  TypedObservable(P&&... a)
+    : m_value{ std::forward<P>(a)... }
   {
   }
 
@@ -139,9 +140,12 @@ public:
   }
 
   std::unique_ptr<TypedObservable<T>> copy() const {
+    // If the compile fails here then you may be copy constructing an
+    // Observable<T> where the type T doesn't have a copy constructor.
     return std::make_unique<TypedObservable<T>>(m_value);
   }
 };
+
 
 template<class T>
 class Observable {
@@ -150,19 +154,34 @@ class Observable {
   std::shared_ptr<TypedObservable<T>> m_inner;
 
 public:
-  Observable()
-    : m_inner{ std::make_shared<TypedObservable<T>>() }
-  {
-  }
+  template<class...>
+  struct CanForward : std::true_type {};
 
-  Observable(T value)
-    : m_inner { std::make_shared<TypedObservable<T>>(std::move(value)) }
+  template<class P>
+  struct CanForward<P>
+    : std::integral_constant<bool, !std::is_same<
+        Observable<T>,
+        std::remove_cv_t<P>>::value
+      >
+  {
+  };
+
+  template<class... P, class = std::enable_if<CanForward<P...>::value>::type>
+  explicit Observable(P&&... a)
+    : m_inner { std::make_shared<TypedObservable<T>>(std::forward<P>(a)...) }
   {
   }
 
   Observable(const Observable& o)
     : m_inner{ o.m_inner->copy() }
   {
+    // Bug in VS2013 means this isn't working for =delete'd copy constructors
+    // so the compile error will be produced further down the stack.
+    // Will catch some cases though.
+    static_assert(
+      std::is_copy_constructible<T>::value,
+      "Copy constructing Observable<T> but T has no copy constructor"
+    );
   }
 
   Observable(Observable&& o)
